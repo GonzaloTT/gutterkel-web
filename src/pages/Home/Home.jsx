abe321f2b0855ba43c48d245e241ace2263b0cbb
GonzaloTT <maganagonzalosegovia@gmail.com>
2026-09-06 16:26:14 -0600

    chore: configure project structure and global styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h1>Gutterkel México</h1>;
+  return (
+    <main>
+      <div className="container">
+        <h1>Gutterkel México</h1>
+
+        <Link to="/soluciones">
+          Soluciones
+        </Link>
+
+        <Link to="/proyectos">
+          Proyectos
+        </Link>
+
+        <Link to="/contacto">
+          Contacto
+        </Link>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
