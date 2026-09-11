a493ebbfa29e57062b41bd3bf0824cc7e1fa996c
GonzaloTT <maganagonzalosegovia@gmail.com>
2026-09-11 16:00:06 -0600

    refactor: expose solution links in header navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,19 +5,21 @@ import solutions from "../../data/solutions";
 
 import gutterkelLogo from "../../assets/images/branding/gutterkel-logo.jpg";
 
-import {
-  ChevronDownIcon,
-} from "../Icons/Icons";
-
 import "./Header.css";
+
+const solutionNavLabels = {
+  "canalon-continuo": "Canalón",
+  "cubre-pretil": "Cubre Pretil",
+  "techos-falsos": "Plafones",
+  "fachada-ventilada": "Fachada",
+  "aislante-termico": "Aislante",
+};
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isSolutionsOpen, setIsSolutionsOpen] = useState(false);
 
   const closeMenu = () => {
     setIsMenuOpen(false);
-    setIsSolutionsOpen(false);
 
     if (document.activeElement instanceof HTMLElement) {
       document.activeElement.blur();
@@ -25,19 +27,7 @@ function Header() {
   };
 
   const toggleMenu = () => {
-    setIsMenuOpen((current) => {
-      const nextState = !current;
-
-      if (!nextState) {
-        setIsSolutionsOpen(false);
-      }
-
-      return nextState;
-    });
-  };
-
-  const toggleSolutions = () => {
-    setIsSolutionsOpen((current) => !current);
+    setIsMenuOpen((current) => !current);
   };
 
   return (
@@ -87,71 +77,37 @@ function Header() {
           >
             <NavLink
               to="/"
+              end
               className="header__link"
               onClick={closeMenu}
             >
               Inicio
             </NavLink>
 
-            <div className="header__nav-item header__nav-item--dropdown" onMouseEnter={() => setIsSolutionsOpen(true)} onMouseLeave={() => setIsSolutionsOpen(false)} >
-              <div className="header__solutions-trigger">
+            <NavLink
+              to="/soluciones"
+              end
+              className="header__link header__link--overview"
+              onClick={closeMenu}
+            >
+              Soluciones
+            </NavLink>
+
+            <div className="header__solutions-nav">
+              {solutions.map((solution) => (
                 <NavLink
-                  to="/soluciones"
-                  className="header__link header__solutions-link"
+                  key={solution.id}
+                  to={solution.href}
+                  className="header__link header__solution-link"
                   onClick={closeMenu}
                 >
-                  Soluciones
-                </NavLink>
-
-                <button
-                  type="button"
-                  className="header__dropdown-toggle"
-                  aria-label={
-                    isSolutionsOpen
-                      ? "Ocultar soluciones"
-                      : "Mostrar soluciones"
+                  {
+                    solutionNavLabels[
+                      solution.id
+                    ] ?? solution.title
                   }
-                  aria-expanded={isSolutionsOpen}
-                  aria-controls="solutions-dropdown"
-                  onClick={toggleSolutions}
-                >
-                  <ChevronDownIcon size={14} />
-                </button>
-              </div>
-
-              <div
-                id="solutions-dropdown"
-                className={`header__dropdown ${
-                  isSolutionsOpen
-                    ? "header__dropdown--open"
-                    : ""
-                }`}
-              >
-                <NavLink
-                  to="/soluciones"
-                  className="header__dropdown-link header__dropdown-link--all"
-                  onClick={closeMenu}
-                >
-                  Ver todas las soluciones
                 </NavLink>
-
-                {solutions.map((solution) => (
-                  <NavLink
-                    key={solution.id}
-                    to={solution.href}
-                    className="header__dropdown-link"
-                    onClick={closeMenu}
-                  >
-                    <span className="header__dropdown-category">
-                      {solution.category}
-                    </span>
-
-                    <span className="header__dropdown-title">
-                      {solution.title}
-                    </span>
-                  </NavLink>
-                ))}
-              </div>
+              ))}
             </div>
 
             <NavLink
